f95f143b5b0ea6857b700b40556ae65b7ca84324
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-09 18:29:11 -0500

    Added Exception Handler to sentiment exception

---
+++ application/src/main/java/co/crisi/exception/SentimentNotFoundHandlerController.java
@@ -0,0 +1,19 @@
+package co.crisi.exception;
+
+import co.crisi.SentimentNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class SentimentNotFoundHandlerController {
+
+
+    @ExceptionHandler(SentimentNotFoundException.class)
+    public @ResponseBody
+    ResponseEntity<String> handleSentimentNotFoundException(SentimentNotFoundException exception) {
+        return ResponseEntity.ok(exception.getMessage());
+    }
+
+}
